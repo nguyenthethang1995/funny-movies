99b3ad1105c7bde35f29b3107569ac599a5a9276
Thang Nguyen The <nguyenthe.thang1995@gmail.com>
2021-12-01 23:51:43 +0700

    change to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_12_01_093114) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "link"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
